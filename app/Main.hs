57230caab3e8dd39b03c30c85c5d02b23a8bae35
John Bolton <jambolo@users.noreply.github.com>
2024-12-05 22:24:20 -1000

    ﻿Day 4

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,20 @@ import System.Environment (getArgs)
 import Day01 (day01_part1, day01_part2)
 import Day02 (day02_part1, day02_part2)
 import Day03 (day03_part1, day03_part2)
+import Day04 (day04_part1, day04_part2)
 
-type PuzzleInputToOutput = String -> IO Int
+type PuzzleInputToInt = String -> IO Int
 
-dayTable :: [(String, PuzzleInputToOutput)]
+dayTable :: [(String, PuzzleInputToInt)]
 dayTable = [
     ("day01_part1", day01_part1),
     ("day01_part2", day01_part2),
     ("day02_part1", day02_part1),
     ("day02_part2", day02_part2),
     ("day03_part1", day03_part1),
-    ("day03_part2", day03_part2)
+    ("day03_part2", day03_part2),
+    ("day04_part1", day04_part1),
+    ("day04_part2", day04_part2)
     ]
 
 main :: IO ()
---
+++ src/Day04.hs
@@ -0,0 +1,59 @@
+module Day04 (
+    day04_part1,
+    day04_part2
+    ) where
+
+import Data.Array
+
+-- loadArray loads an array from a list of lists
+createPuzzle :: [[Char]] -> Array (Int, Int) Char
+createPuzzle a =
+    let nRows = length a
+        nCols = length (head a)
+        bnds = ((0, 0), (nRows - 1, nCols - 1))
+        elements = [((i, j), a !! i !! j) | i <- [0 .. nRows - 1], j <- [0 .. nCols - 1]]
+    in array bnds elements
+
+checkXmas :: Array (Int, Int) Char -> (Int, Int) -> (Int, Int) -> Bool
+checkXmas puzzle (i, j) (di, dj) =  puzzle ! (i + 0 * di, j + 0 * dj) == 'X' &&
+                                    puzzle ! (i + 1 * di, j + 1 * dj) == 'M' &&
+                                    puzzle ! (i + 2 * di, j + 2 * dj) == 'A' &&
+                                    puzzle ! (i + 3 * di, j + 3 * dj) == 'S'
+
+
+findMatches :: Array (Int, Int) Char -> (Int, Int) -> ((Int, Int), (Int, Int)) -> [Bool]
+findMatches puzzle d ((top, left), (bottom, right)) = [checkXmas puzzle (i, j) d | i <- [top .. bottom], j <- [left .. right]]
+
+countMatches :: Array (Int, Int) Char -> (Int, Int) -> ((Int, Int), (Int, Int)) -> Int
+countMatches puzzle d bnds = length . filter id $ findMatches puzzle d bnds
+
+day04_part1 :: String -> IO Int
+day04_part1 input = do
+    let puzzle = createPuzzle $ lines input
+    let ((top, left), (bottom, right)) = bounds puzzle
+    let total = countMatches puzzle ( 0,  1) ((top    , left    ), (bottom    , right - 3)) +
+                countMatches puzzle (-1,  1) ((top + 3, left    ), (bottom    , right - 3)) +
+                countMatches puzzle (-1,  0) ((top + 3, left    ), (bottom    , right    )) +
+                countMatches puzzle (-1, -1) ((top + 3, left + 3), (bottom    , right    )) +
+                countMatches puzzle ( 0, -1) ((top    , left + 3), (bottom    , right    )) +
+                countMatches puzzle ( 1, -1) ((top    , left + 3), (bottom - 3, right    )) +
+                countMatches puzzle ( 1,  0) ((top    , left    ), (bottom - 3, right    )) +
+                countMatches puzzle ( 1,  1) ((top    , left    ), (bottom - 3, right - 3))
+    return total
+
+checkX :: Array (Int, Int) Char -> (Int, Int) -> Bool
+checkX puzzle (i, j) = puzzle ! (i + 0, j + 0) == 'A' &&
+                       (puzzle ! (i - 1, j - 1) == 'M' && puzzle ! (i + 1, j + 1) == 'S' ||
+                        puzzle ! (i - 1, j - 1) == 'S' && puzzle ! (i + 1, j + 1) == 'M') &&
+                       (puzzle ! (i - 1, j + 1) == 'M' && puzzle ! (i + 1, j - 1) == 'S' ||
+                        puzzle ! (i - 1, j + 1) == 'S' && puzzle ! (i + 1, j - 1) == 'M')
+
+findXs :: Array (Int, Int) Char -> ((Int, Int), (Int, Int)) -> [Bool]
+findXs puzzle ((top, left), (bottom, right)) = [checkX puzzle (i, j) | i <- [top .. bottom], j <- [left .. right]]
+
+day04_part2 :: String -> IO Int
+day04_part2 input = do
+    let puzzle = createPuzzle $ lines input
+    let ((top, left), (bottom, right)) = bounds puzzle
+    let total = length . filter id $ findXs puzzle ((top + 1, left + 1), (bottom - 1, right - 1))
+    return total
